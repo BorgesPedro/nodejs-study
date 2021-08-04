52e96e991feb6403e6d6f3a6d5b53f130f6ee487
Pedro Borges <pedrohenriqueborges2003@gmail.com>
2021-08-04 00:07:58 -0300

    Upgrade da API e novo modelo do produtos.json

--- api/index.js
+++ api/index.js
@@ -1,24 +1,38 @@
 const http = require('http')
-const URL = require('url')
 const fs = require('fs')
 const path = require('path')
 const data = require('./produtos.json')
 
-function writeProduto(cb) {
+function writeFile(cb){
     fs.writeFile(
         path.join(__dirname, "produtos.json"),
         JSON.stringify(data, null, 2),
         err => {
-            if(err) throw err
+            if (err) throw err
 
             cb(JSON.stringify({message: "ok"}))
         }
+        
     )
 }
 
-http.createServer((req, res) => {
-   res.writeHead(200, {
-       'Access-Control-Allow-Origin': '*'
-   })
+http.createServer((req,res) => {
+    const url = new URL(`http://localhost:3000${req.url}`)
 
-}).listen(3000)
+    const codigo = url.searchParams.get('codigo')
+    const nome = url.searchParams.get('nome')
+    const date = url.searchParams.get('data')
+    const del = url.searchParams.get('del')
+
+
+    if(!codigo || !nome || !data)
+        return res.end(JSON.stringify(data))
+
+    if(del){
+        data.produtos = data.produtos.filter(item => String(item.codigo) !== String(codigo))
+        return writeFile((message) => {res.end(message)})
+    }
+    
+    data.produtos.push({codigo, nome, date})
+    return writeFile((message) => {res.end(message)})
+}).listen(3000, () => {console.log("Api está rodando")})
